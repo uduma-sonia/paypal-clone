96ffbb5437e6934813a737ddd8a648e5e0456004
Sonia Uduma <sohnyauduma@gmail.com>
2021-11-16 13:39:58 +0100

    mobile responsiveness

--- components/footer/footerBottom.js
+++ components/footer/footerBottom.js
@@ -2,50 +2,49 @@ export default function FooterBottom() {
   return (
     <section style={{ padding: "50px 0px" }}>
       <div className="container mx-auto px-8 md:px-28">
-        <div className="text-dark font-semibold text-xs border border-dotted border-t-0 border-l-0 border-r-0 border-gray-600 pb-3 mb-3">
-          <a className="cursor-pointer border border-gray-900 ">
-            Help and Contact
-          </a>
+        <div className="text-dark font-semibold text-xs md:border border-dotted md:border-t-0 md:border-l-0 md:border-r-0 border-gray-600 pb-3 mb-3">
+          <div className="md:w-96 w-full flex md:justify-between justify-evenly flex-wrap">
+            <a className="cursor-pointer w-36 md:w-auto">Help and Contact</a>
 
-          <a className="mx-5 cursor-pointer">Fees</a>
+            <a className="cursor-pointer w-20 md:w-auto">Fees</a>
 
-          <a className="cursor-pointer">Security</a>
-          <br />
+            <a className="cursor-pointer w-20 md:w-auto">Security</a>
 
-          <a className="mx-5 cursor-pointer">Features</a>
+            <a className="cursor-pointer mt-4 md:mt-0">Features</a>
 
-          <a className="cursor-pointer">Shop</a>
+            <a className="cursor-pointer mt-4 md:mt-0">Shop</a>
+          </div>
         </div>
 
         <div
-          className="text-dark font-semibold opacity-80 flex justify-between flex-col md:flex-row"
+          className="text-dark font-semibold opacity- flex justify-between flex-col md:flex-row"
           style={{ fontSize: "11px" }}
         >
-          <div>
-            <a className="cursor-pointer">About</a>
+          <div className="w-80 flex md:justify-between justify-evenly flex-wrap">
+            <a className="cursor-pointer w-16 md:w-auto">About</a>
 
-            <a className="mx-5 cursor-pointer">Newsrooms</a>
+            <a className="cursor-pointer w-20 md:w-auto">Newsrooms</a>
 
-            <a className="cursor-pointer">jobs</a>
+            <a className="cursor-pointer w-16 md:w-auto">jobs</a>
 
-            <a className="mx-5 cursor-pointer">Sitemap</a>
+            <a className="cursor-pointer w-16 md:w-auto">Sitemap</a>
 
-            <a className="cursor-pointer">Developers</a>
+            <a className="cursor-pointer mt-2 md:mt-0">Developers</a>
           </div>
 
-          <div className="border border-gray-900 ">
+          <div className="md:w-96 flex md:justify-between justify-evenly flex-wrap mt-5 md:mt-0">
             <a>© 1999–2021 All rights reserved.</a>
 
             <a className="mx-5 cursor-pointer">Accessibility</a>
 
             <a className="cursor-pointer">Privacy</a>
 
-            <a className="mx-5 cursor-pointer">Legal</a>
+            <a className="mx-5 mt-2 md:mt-0 cursor-pointer">Legal</a>
           </div>
         </div>
 
         <p
-          className="text-dark font-bold mt-5 text-center md:text-left"
+          className="text-dark font-semibold mt-5 text-center md:text-left opacity-"
           style={{ fontSize: "11px" }}
         >
           PayPal Pte. Ltd. (“3PL”) has applied for a license under the Singapore
